a8dc27f371ba08541635c1d1e76f6503cfe93eda
ZWCWORK <mr.z.1996@outlook.com>
2021-05-26 10:33:07 +0800

    change default version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_version(fname):
         with open(fname, 'r') as f:
             version = f.readline().strip()
     else:
-        version = 'alpha'
+        version = 'rx'
 
     return version
 
